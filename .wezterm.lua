24f88529aee59c8153c38634dc819be030edc730
sonarAIT <sonarmikumiku@gmail.com>
2023-06-05 05:07:28 +0900

    [feat] wezterm font config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,3 +1,26 @@
--- NEVER CHANGE
-return require("init")
+local wezterm = require 'wezterm'
 
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'AdventureTime'
+config.font = wezterm.font("Scientifica Nerd Font", {weight="Medium", stretch="Normal", style="Normal"})
+config.font_rules = { {
+    italic = true,
+    font = wezterm.font_with_fallback {
+      { family = "Scientifica Nerd Font", weight="Medium", stretch="Normal", style="Normal"}
+    },
+  },
+  {
+    intensity = "Bold",
+    font = wezterm.font_with_fallback {
+      { family = "Scientifica Nerd Font", weight="Medium", stretch="Normal", style="Normal"}
+    },
+  },
+}
+config.font_size = 16
+
+return config
