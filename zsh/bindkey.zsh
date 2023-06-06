2924cf447dcdb983d26f237318ad214610576467
sonarAIT <sonarmikumiku@gmail.com>
2023-06-06 18:14:01 +0900

    [add] cursol move shortcut

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -1,2 +1,16 @@
-# word delete
-bindkey '^H' backward-delete-word
+# for WSL2
+if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+  # word delete
+  bindkey '^H' backward-delete-word
+  # cursol move
+  bindkey '^B' backward-word
+  bindkey '^W' forward-word
+fi
+
+if [[ "$(uname)" == 'Darwin' ]]; then
+  # word delete
+  bindkey '^H' backward-delete-word
+  # cursol move
+  bindkey '^B' backward-word
+  bindkey '^W' forward-word
+fi
