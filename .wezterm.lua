7836931de518c725d8883214beb42f44a7bd946c
sonarAIT <sonarmikumiku@gmail.com>
2023-06-07 11:23:58 +0900

    [change] cursol move by cmd

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+-- theme config
 config.color_scheme = 'MaterialDesignColors'
 config.font = wezterm.font("Scientifica Nerd Font", {weight="Medium", stretch="Normal", style="Normal"})
 config.font_rules = {
@@ -23,5 +24,34 @@ config.font_rules = {
   },
 }
 config.font_size = 16
+
+-- key config
+local act = wezterm.action
+config.keys = {
+  {
+    key = "LeftArrow",
+    mods = "CMD",
+    action = act.SendKey {
+      key = "b",
+      mods = "CTRL",
+    },
+  },
+  {
+    key = "RightArrow",
+    mods = "CMD",
+    action = act.SendKey {
+      key = "f",
+      mods = "CTRL",
+    },
+  },
+  {
+    key = "Backspace",
+    mods = "CMD",
+    action = act.SendKey {
+      key = "Backspace",
+      mods = "CTRL",
+    },
+  },
+}
 
 return config
--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -3,8 +3,6 @@ if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
   # word delete
   bindkey '^H' backward-delete-word
   # cursol move
-  bindkey '^[b' backward-char
-  bindkey '^[w' forward-char
   bindkey '^B' backward-word
   bindkey '^W' forward-word
 fi
@@ -13,8 +11,6 @@ if [[ "$(uname)" == 'Darwin' ]]; then
   # word delete
   bindkey '^H' backward-delete-word
   # cursol move
-  bindkey '^[b' backward-char
-  bindkey '^[w' forward-char
   bindkey '^B' backward-word
   bindkey '^W' forward-word
 fi
